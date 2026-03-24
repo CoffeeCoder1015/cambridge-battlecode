4a53ec2859526853db9db436f9deeb52a51dec7a
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-03-23 18:05:16 -0700

    mod: moving symmetry into its own file

---
+++ bots/k_learning/builderbot/navigation.py
@@ -0,0 +1,100 @@
+from enum import Enum
+
+from cambc import Controller, Position, Team, Environment
+
+class LUT(Enum):
+    UNEXPLORED = 0
+    EMPTY = 1
+    ORE_TITANIUM = 2
+    WALL = 3
+    ORE_AXIONITE = 4
+
+class SymmetryAnalyzer:
+    def __init__(self, w: int, h: int, ally_core_pos: Position):
+        self.w = w
+        self.h = h
+        self.ally_core_pos = ally_core_pos
+        self.map_lut = [[LUT.UNEXPLORED] * w for _ in range(h)]
+        self.possible_symmetries = ["horizontal", "vertical", "rotational"]
+        self.eliminate_core_overlap()
+    
+    def eliminate_core_overlap(self):
+        """Early elimination based on core position overlap."""
+        cx, cy = self.ally_core_pos.x, self.ally_core_pos.y
+        overlap_x = abs(2 * cx - (self.w - 1)) <= 2
+        overlap_y = abs(2 * cy - (self.h - 1)) <= 2
+        
+        if overlap_x and "horizontal" in self.possible_symmetries:
+            self.possible_symmetries.remove("horizontal")
+        if overlap_y and "vertical" in self.possible_symmetries:
+            self.possible_symmetries.remove("vertical")
+        if overlap_x and overlap_y and "rotational" in self.possible_symmetries:
+            self.possible_symmetries.remove("rotational")
+
+    def update_symmetry(self, ct: Controller, nearby_tiles: list[Position], nearby_units: list[int]):
+        cx, cy = self.ally_core_pos.x, self.ally_core_pos.y
+        
+        # 1. Deterministic Core-based Symmetry Confirmation
+        enemy_id = 2 if ct.get_team() == Team.A else 1
+        if enemy_id in nearby_units:
+            enemy_pos = ct.get_position(enemy_id)
+            ex, ey = enemy_pos.x, enemy_pos.y
+            
+            confirmed_sym = None
+            if ex == self.w - 1 - cx and ey == cy:
+                confirmed_sym = "horizontal"
+            elif ex == cx and ey == self.h - 1 - cy:
+                confirmed_sym = "vertical"
+            elif ex == self.w - 1 - cx and ey == self.h - 1 - cy:
+                confirmed_sym = "rotational"
+            
+            if confirmed_sym and confirmed_sym in self.possible_symmetries:
+                self.possible_symmetries = [confirmed_sym]
+                return # Core found, symmetry is deterministic
+        
+        # 2. Environment-based POI Matching (if not yet deterministic)
+        if len(self.possible_symmetries) <= 1:
+            return
+
+        for pos in nearby_tiles:
+            if self.map_lut[pos.y][pos.x] != LUT.UNEXPLORED:
+                continue
+                
+            env = ct.get_tile_env(pos)
+            if env == Environment.ORE_TITANIUM:
+                val = LUT.ORE_TITANIUM
+            elif env == Environment.WALL:
+                val = LUT.WALL
+            elif env == Environment.ORE_AXIONITE:
+                val = LUT.ORE_AXIONITE
+            else:
+                val = LUT.EMPTY
+                
+            if val.value > LUT.EMPTY.value:
+                for sym in self.possible_symmetries[:]:
+                    sym_x, sym_y = pos.x, pos.y
+                    if sym == "horizontal":
+                        sym_x = self.w - 1 - pos.x
+                    elif sym == "vertical":
+                        sym_y = self.h - 1 - pos.y
+                    elif sym == "rotational":
+                        sym_x = self.w - 1 - pos.x
+                        sym_y = self.h - 1 - pos.y
+                        
+                    if 0 <= sym_x < self.w and 0 <= sym_y < self.h:
+                        sym_val = self.map_lut[sym_y][sym_x]
+                        if sym_val != LUT.UNEXPLORED and sym_val != val:
+                            self.possible_symmetries.remove(sym)
+                            
+            self.map_lut[pos.y][pos.x] = val
+
+    def draw_debug(self, ct: Controller):
+        current_pos = ct.get_position()
+        cx, cy = self.ally_core_pos.x, self.ally_core_pos.y
+        for sym in self.possible_symmetries:
+            if sym == "horizontal":
+                ct.draw_indicator_line(current_pos, Position(self.w - 1 - cx, cy), 255, 165, 0)
+            elif sym == "vertical":
+                ct.draw_indicator_line(current_pos, Position(cx, self.h - 1 - cy), 255, 165, 0)
+            elif sym == "rotational":
+                ct.draw_indicator_line(current_pos, Position(self.w - 1 - cx, self.h - 1 - cy), 255, 165, 0)
