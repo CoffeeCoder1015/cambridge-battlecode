ac859b55eab21430c450219e1478648e247aea6e
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-03-30 08:39:41 -0700

    reenableing multi bot spawning

--- bots/k_learning/core/__init__.py
+++ bots/k_learning/core/__init__.py
@@ -39,8 +39,6 @@ class Core:
         res = ct.get_global_resources()
         titanium = res[0]
         rnd = ct.get_current_round()
-        if rnd > 0:
-            return
         
         # 0.1 Adaptive Expense Tracking
         # If the swarm bottomed out the bank entirely (< 3 Ti for a conveyor),
